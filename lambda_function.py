54fae871ffcb25b55762394d42ef609105e5ff0d
Tamim <farman.arefin1@outlook.com>
2022-12-29 00:08:04 +0600

    First commit

--- lambda_function.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def lambda_handler(event,context):
-    return{
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!........(Modified v.02)')
-
-        print("Farman Arefin Tamim")
-    }
